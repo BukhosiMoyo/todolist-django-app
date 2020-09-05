03ee2f9d6a8c3f56517884025a53f4ed03a74473
Bukhosi Moyo <Bukhosizimcode@gmail.com>
2020-09-05 10:38:17 +0200

    Created the first view to render a form and a list of todos

---
+++ tasks/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import *
+from .forms import *
+
+# Create your views here.
+
+def  index(request):
+
+    tasks = Task.objects.all()
+    form = TaskForm()
+
+    context= {'tasks':tasks, 'form':form}
+
+    return render(request, "tasks/list.html", context)
